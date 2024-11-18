490c5228ad15a6af48181649d8124e219ca928ab
Yuliana-Alcala <alcalabyulianac@gmail.com>
2024-11-18 10:16:01 +0100

    create model BmiModel

--- src/main/java/Models/BmiModel.java
+++ src/main/java/Models/BmiModel.java
@@ -2,4 +2,48 @@ package Models;
 
 public class BmiModel {
 
+    private PersonModel person;
+
+    public BmiModel(PersonModel person){
+        this.person = person;
+       
+    }
+
+    public double calculateBMI(){
+        double weight = person.getWeight();
+        double height = person.getHeight();
+
+       double bmi =weight / (Math.pow(height,2));   
+       return bmi;
+
+    }
+
+    public String giveResultsBMI(double bmi){
+        if (bmi < 16){
+            return "Severe thinness";
+        }else if(bmi < 17){
+            return "Moderate thinness";
+
+        }else if(bmi < 18.5){
+            return "Mild thinness";
+
+        }else if(bmi < 25){
+            return "Normal weight";
+
+        }else if(bmi < 30){
+            return "Overweight";
+
+        }else if(bmi < 35){
+            return "Mild obesity";
+
+        }else if(bmi < 40){
+            return "Moderate obesity";
+
+        }else{
+            return "Morbid obesity";
+
+        }
+
+    }
+
 }
